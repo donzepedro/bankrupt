4ed6729a6b7edc32e2a66f9b83d03fed0178a910
zepedro <zepedro@yandex.ru>
2021-07-31 14:33:29 +0000

    local remote commit

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+define('ROOT', str_replace('web', '', __DIR__));
+define('HOME', '/arbitr-manager/index?');
+define('DOMAIN','http://atra-centr.ru/');
+define('MANAGERS_IMG_FOLDER', '../web/img/managers_profile_img/');
+require 'functions.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../config/web.php';
+
+(new yii\web\Application($config))->run();
