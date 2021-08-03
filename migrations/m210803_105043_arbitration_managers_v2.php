d501988317ce45db22db73239c3ea3321049def6
zepedro <zepedro@yandex.ru>
2021-08-03 14:35:57 +0300

    i changed the DB Table for AM. Add new fields debtor and bankrupt cadegories

--- migrations/m210803_105043_arbitration_managers_v2.php
+++ migrations/m210803_105043_arbitration_managers_v2.php
@@ -3,15 +3,12 @@
 use yii\db\Migration;
 
 /**
- * Class m210623_074851_arbitration_managers
+ * Class m210803_105043_arbitration_managers_v2
  */
-class m210624_115103_arbitration_managers extends Migration {
-
-    /**
-     * {@inheritdoc}
-     */
-    public function safeUp() {
-        $this->execute("CREATE TABLE arbitration_manager
+class m210803_105043_arbitration_managers_v2 extends Migration
+{
+    public function safeUp(){
+     $this->execute("CREATE TABLE arbitration_manager
                             (
                              id                       int NOT NULL  AUTO_INCREMENT,
                              lname                    varchar(20) NOT NULL ,
@@ -25,13 +22,14 @@ class m210624_115103_arbitration_managers extends Migration {
                              government_secret_access tinyint(1) NULL DEFAULT NULL ,
                              legal_phys               int NULL DEFAULT NULL COMMENT '0-phys/1-legal' ,
                              SRO_AM_name              varchar(100) NOT NULL ,
-                             categories               tinyint(2) NOT NULL COMMENT 'At this column user have to choose what categories he will be accept legal or physical' ,
+                             bankrupt_categoties      tinyint(2) NOT NULL COMMENT 'categories of clients(bankrupt/creditor) 0-phys 1-legal 3-both' ,
+                             debtor_categories          tinyint(2) NOT NULL COMMENT 'categories of debtors(bankrupt/creditor) 0-phys 1-legal 3-both' ,
                              count_of_procedure_phys  int NOT NULL ,
                              count_of_procedure_legal int NOT NULL ,
                              procedure_time_average   float NOT NULL COMMENT 'procedure_time_average months' ,
                              start_date               date NOT NULL ,
                              end_date                 date NULL ,
-                             path_to_img              varchar(30) NULL ,
+                             path_to_img              varchar(100) NULL ,
 
                             PRIMARY KEY (id)
                             ) ENGINE=INNODB COLLATE=utf8mb4_0900_ai_ci;
@@ -45,19 +43,4 @@ class m210624_115103_arbitration_managers extends Migration {
         
         return $this->dropTable("arbitration_manager");
     }
-
-    /*
-      // Use up()/down() to run migration code without a transaction.
-      public function up()
-      {
-
-      }
-
-      public function down()
-      {
-      echo "m210624_115103_arbitration_managers cannot be reverted.\n";
-
-      return false;
-      }
-     */
 }
--- models/ArbitrationManager.php
+++ models/ArbitrationManager.php
@@ -17,13 +17,16 @@ use Yii;
  * @property int|null $phone_number
  * @property string|null $job_region
  * @property int|null $government_secret_access
+ * @property int|null $legal_phys 0-phys/1-legal
  * @property string $SRO_AM_name
- * @property int $categories At this column user have to choose what categories he will be accept legal or physical
+ * @property int $bankrupt_categoties categories of clients(bankrupt/creditor) 0-phys 1-legal 3-both
+ * @property int $debtor_categories categories of debtors(bankrupt/creditor) 0-phys 1-legal 3-both
  * @property int $count_of_procedure_phys
  * @property int $count_of_procedure_legal
  * @property float $procedure_time_average procedure_time_average months
  * @property string $start_date
  * @property string|null $end_date
+ * @property string|null $path_to_img
  *
  * @property SROAMInformation[] $sROAMInformations
  * @property Education[] $educations
@@ -34,7 +37,6 @@ class ArbitrationManager extends \yii\db\ActiveRecord
     /**
      * {@inheritdoc}
      */
-     
     public static function tableName()
     {
         return 'arbitration_manager';
@@ -46,17 +48,15 @@ class ArbitrationManager extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['lname', 'fname', 'SRO_AM_name', 'categories', 'count_of_procedure_phys', 'count_of_procedure_legal', 'procedure_time_average', 'start_date'], 'required'],
+            [['lname', 'fname', 'SRO_AM_name', 'bankrupt_categoties', 'debtor_categories', 'count_of_procedure_phys', 'count_of_procedure_legal', 'procedure_time_average', 'start_date'], 'required'],
             [['birth_date', 'start_date', 'end_date'], 'safe'],
-            [['inn', 'phone_number', 'government_secret_access', 'categories', 'count_of_procedure_phys', 'count_of_procedure_legal'], 'integer'],
+            [['inn', 'phone_number', 'government_secret_access', 'legal_phys', 'bankrupt_categoties', 'debtor_categories', 'count_of_procedure_phys', 'count_of_procedure_legal'], 'integer'],
             [['procedure_time_average'], 'number'],
-            [['inn'],'string','max'=>12],
             [['lname', 'fname', 'mname'], 'string', 'max' => 20],
             [['post_addr'], 'string', 'max' => 255],
             [['job_region'], 'string', 'max' => 50],
             [['SRO_AM_name'], 'string', 'max' => 100],
-            
-           
+            [['path_to_img'], 'string', 'max' => 100],
         ];
     }
 
@@ -76,13 +76,16 @@ class ArbitrationManager extends \yii\db\ActiveRecord
             'phone_number' => 'Phone Number',
             'job_region' => 'Job Region',
             'government_secret_access' => 'Government Secret Access',
+            'legal_phys' => 'Legal Phys',
             'SRO_AM_name' => 'Sro Am Name',
-            'categories' => 'Categories',
+            'bankrupt_categoties' => 'Bankrupt Categoties',
+            'debtor_categories' => 'Debtor Categories',
             'count_of_procedure_phys' => 'Count Of Procedure Phys',
             'count_of_procedure_legal' => 'Count Of Procedure Legal',
             'procedure_time_average' => 'Procedure Time Average',
             'start_date' => 'Start Date',
             'end_date' => 'End Date',
+            'path_to_img' => 'Path To Img',
         ];
     }
 
@@ -115,13 +118,4 @@ class ArbitrationManager extends \yii\db\ActiveRecord
     {
         return $this->hasMany(ForeignLanguage::className(), ['id_am' => 'id']);
     }
-    
-    public function innlength($attribute)
-    {
-        if(strlen($this->attribute) > 11 ){
-
-            $this->addError($attribute,'inn must be short then 11 symbols');
-        }
-    }
-    
-}   
+}
